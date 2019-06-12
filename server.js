f14f0e30c3eb50183d1a2a69a59dd32846eceaa1
fcarlone <frankcarlone@gmail.com>
2019-06-12 11:45:46 -0400

    Config server.js file

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+// Port 
+let PORT = 3000;
+
+// Require all models
+//const db = require("./models");
+
+// Initialize Express
+const app = express();
+
+// Middleware
+
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Static folder (public)
+app.use(express.static("public"));
+
+// Connect to Mongo DB
+mongoose.connect("mongodb://localhost/mongo-scraper", { useNewUrlParser: true });
+
+
+// Start the sever
+app.listen(PORT, function () {
+  console.log(`Listening on PORT: ${PORT}`);
+});
