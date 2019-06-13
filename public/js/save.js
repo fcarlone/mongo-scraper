b5fac37f18124adab84ae8a26f8e996524530486
fcarlone <frankcarlone@gmail.com>
2019-06-13 16:25:52 -0400

    Display saved articles on web app

--- public/js/save.js
+++ public/js/save.js
@@ -3,5 +3,12 @@ console.log('save.js')
 $.getJSON("/saves", function (data) {
   data.forEach((article) => {
     console.log('saved article', article)
+    $("#saved-article").append(
+      `<h3 data-id="${article._id}"> ${article.headline}
+      <button type="button" class="btn btn-warning" article-id="${article._id}">Article Notes</button>
+       <button type="button" class="btn btn-danger" article-id="${article._id}">Delete Article</button>
+       <h5> ${article.summary} 
+       <a href="http://www.nytimes.com${article.link}" target="_blank">View Article</a>`
+    )
   })
 })
