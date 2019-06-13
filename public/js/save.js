0f1da7bd0a73070d7ccadd2aeb00c2a1fb15cb67
fcarlone <frankcarlone@gmail.com>
2019-06-13 16:43:06 -0400

    Add delete and note buttons to saved articles

--- public/js/save.js
+++ public/js/save.js
@@ -5,10 +5,19 @@ $.getJSON("/saves", function (data) {
     console.log('saved article', article)
     $("#saved-article").append(
       `<h3 data-id="${article._id}"> ${article.headline}
-      <button type="button" class="btn btn-warning" article-id="${article._id}">Article Notes</button>
-       <button type="button" class="btn btn-danger" article-id="${article._id}">Delete Article</button>
+       <button type="button" class="btn btn-warning" article-id="${article._id}">Article Notes</button>
+       <button type="button" class="btn btn-delete btn-danger" article-id="${article._id}">Delete Article</button>
        <h5> ${article.summary} 
        <a href="http://www.nytimes.com${article.link}" target="_blank">View Article</a>`
     )
-  })
+  });
+});
+
+// Handle delete article on-click event
+$(document).on("click", ".btn-delete", function () {
+  console.log("delete button clicked")
+
+  // Get article id
+  let articleId = $(this).attr("article-id")
+  console.log("Article ID", articleId)
 })
