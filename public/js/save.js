bb83b03bdfd9c879be4448e6e3c6cdc0a2ad1b35
fcarlone <frankcarlone@gmail.com>
2019-06-14 09:16:29 -0400

    Delete article from saved list

--- public/js/save.js
+++ public/js/save.js
@@ -5,7 +5,7 @@ $.getJSON("/saves", function (data) {
     console.log('saved article', article)
     $("#saved-article").append(
       `<h3 data-id="${article._id}"> ${article.headline}
-       <button type="button" class="btn btn-warning" article-id="${article._id}">Article Notes</button>
+       <button type="button" class="btn btn-note btn-warning" article-id="${article._id}">Article Notes</button>
        <button type="button" class="btn btn-delete btn-danger" article-id="${article._id}">Delete Article</button>
        <h5> ${article.summary} 
        <a href="http://www.nytimes.com${article.link}" target="_blank">View Article</a>`
@@ -16,8 +16,12 @@ $.getJSON("/saves", function (data) {
 // Handle delete article on-click event
 $(document).on("click", ".btn-delete", function () {
   console.log("delete button clicked")
-
   // Get article id
   let articleId = $(this).attr("article-id")
   console.log("Article ID", articleId)
-})
+
+  $.get(`/delete/${articleId}`)
+    .then(function (article) {
+      console.log('article deleted', article)
+    });
+});
--- routes/saved-routes.js
+++ routes/saved-routes.js
@@ -15,14 +15,35 @@ module.exports = function (app) {
       .catch(function (err) {
         res.json(err);
       });
-  })
+  });
 
-
+  // POST route to save article to MongoDB
   app.post("/saved", function (req, res) {
     db.Save.create(req.body)
       .then(function (dbSave) {
         console.log('saved article', dbSave)
         res.json(dbSave)
       })
-  })
+      .catch(function (err) {
+        res.json(err);
+      });
+  });
+
+  // GET route to delete article from MongoDB
+  app.get("/delete/:id", function (req, res) {
+    db.Save.remove({
+      _id: req.params.id
+    },
+      function (error, removed) {
+        if (error) {
+          console.log(error)
+          res.send(error)
+        } else {
+          console.log("removed from MongoDB", removed)
+          res.send(removed)
+        }
+      });
+  });
+
+
 }
